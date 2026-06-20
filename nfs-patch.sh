fdadfb7f7efad921d965dd917fdc3cc16a04a839
v0lk3n <volken_haxor@protonmail.com>
2026-06-20 17:53:00 +0200

    Add nfs patch

---
+++ nfs-patch.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+curl https://github.com/Neternels/android_kernel_xiaomi_sunny/commit/f51823ab5c82bf40ca879204f724bcae7677a371.patch | git am
+curl https://github.com/Neternels/android_kernel_xiaomi_sunny/commit/940bb7a0666e7b75202e756cf14fa410451c0dbf.patch | git am
+curl https://github.com/Neternels/android_kernel_xiaomi_sunny/commit/b05958e0a821eb73c9999d84f3fc27caa747d6a9.patch | git am
+curl https://github.com/Neternels/android_kernel_xiaomi_sunny/commit/c10d5e6648ef34f55185bf38ccfcc314c2cba8cf.patch | git am
+curl https://github.com/Neternels/android_kernel_xiaomi_sunny/commit/8b792f2227320fcbf998447437de96e1575d1972.patch | git am
+curl https://github.com/Neternels/android_kernel_xiaomi_sunny/commit/79ed6315cc54929ba9d17b0fa38621c0e50171a8.patch | git am
+curl https://github.com/Neternels/android_kernel_xiaomi_sunny/commit/54540daafe23884f072a029cdd8be0842fcfcfbf.patch | git am
+git push
